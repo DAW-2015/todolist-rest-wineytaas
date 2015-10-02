996b4915c4a5fd93fbcb3d495c3d6751d0888b1d
Wisney <wisneytaas@gmail.com>
2015-10-02 15:32:55 -0300

    Categoria

--- index.php
+++ index.php
@@ -4,7 +4,8 @@ require 'Slim/Slim.php';
 \Slim\Slim::registerAutoloader();
 
 require 'connection.php';
-require 'clienteDAO.php';
+require 'categoriaDAO.php';
+require 'tarefaDAO.php';
 require 'usuarioDAO.php';
 
 
@@ -55,6 +56,104 @@ $app->delete('/usuarios/:id', function($id) {
         echo "{'message':'Usuario excluído'}";
     } else {
         echo "{'message':'Erro ao excluir usuario'}";
+    }
+});
+
+//---------------------------CATEGORIA-----------------------------------------
+
+$app->get('/categorias/:id', function ($id) {
+    //recupera o cliente
+    $categoria = CategoriaDAO::getCategoriaByID($id);
+    
+    echo json_encode($categoria);
+});
+
+$app->get('/categorias', function() {
+    // recupera todos os clientes
+    $categorias = CategoriaDAO::getAll();
+    echo json_encode($categorias);
+});
+
+$app->post('/categorias', function() {
+    // recupera o request
+    $request = \Slim\Slim::getInstance()->request();
+
+    // insere o cliente
+    $novoCategoria = json_decode($request->getBody());
+    $novoCategoria = CategoriaDAO::addCategoria($novoCategoria);
+
+    echo json_encode($novoCategoria);
+});
+
+$app->put('/categorias/:id', function ($id) {
+    // recupera o request
+    $request = \Slim\Slim::getInstance()->request();
+
+    // atualiza o cliente
+    $categoria = json_decode($request->getBody());
+    $categoria = CategoriaDAO::updateCategoria($categoria, $id);
+
+    echo json_encode($categoria);
+});
+
+$app->delete('/categorias/:id', function($id) {
+    // exclui o cliente
+    $isDeleted = CategoriaDAO::deleteCategoria($id);
+
+    // verifica se houve problema na exclusão
+    if ($isDeleted) {
+        echo "{'message':'Categoria excluído'}";
+    } else {
+        echo "{'message':'Erro ao excluir categoria'}";
+    }
+});
+
+//---------------------------TAREFA-----------------------------------------
+
+$app->get('/tarefas/:id', function ($id) {
+    //recupera o cliente
+    $tarefa = TarefaDAO::getTarefaByID($id);
+    
+    echo json_encode($categoria);
+});
+
+$app->get('/tarefas', function() {
+    // recupera todos os clientes
+    $tarefas = TarefaDAO::getAll();
+    echo json_encode($tarefas);
+});
+
+$app->post('/tarefas', function() {
+    // recupera o request
+    $request = \Slim\Slim::getInstance()->request();
+
+    // insere o cliente
+    $novoTarefa = json_decode($request->getBody());
+    $novoTarefa = TarefaDAO::addTarefa($novoTarefa);
+
+    echo json_encode($novoTarefa);
+});
+
+$app->put('/tarefas/:id', function ($id) {
+    // recupera o request
+    $request = \Slim\Slim::getInstance()->request();
+
+    // atualiza o cliente
+    $tarefa = json_decode($request->getBody());
+    $tarefa = TarefaDAO::updateTarefa($tarefa, $id);
+
+    echo json_encode($tarefa);
+});
+
+$app->delete('/tarefas/:id', function($id) {
+    // exclui o cliente
+    $isDeleted = TarefaDAO::deleteTarefa($id);
+
+    // verifica se houve problema na exclusão
+    if ($isDeleted) {
+        echo "{'message':'Tarefa excluído'}";
+    } else {
+        echo "{'message':'Erro ao excluir tarefa'}";
     }
 });
 
--- readme.md
+++ readme.md
@@ -28,3 +28,60 @@
     1. parametro: **id** do usuario desejado
     2. body: NULL
     3. return: **Usuario excluído**, caso true. **Erro ao excluir usuario**, case false
+
+
+##Categoria
+
+
+1.  `GET /categorias/:id`
+    1. parametro: **id** do categoria desejada
+    2. body: NULL
+    3. return: O **id** , **nome** , **usuario_id**, **usuario** (**id** , **nome**, **email**, **login**, **senha**) da categoria selecionada
+
+2.  `GET /categorias`
+    1. parametro: NULL
+    2. body: NULL
+    3. return: Os **id** , **nome** , **usuario_id**, **usuario** (**id** , **nome**, **email**, **login**, **senha**) de todas as categorias
+
+3.  `POST /categorias`
+    1. parametro: NULL
+    2. body: { "nome": "NOME_CATEGORIA", "usuario_id": "USUARIO_ID_CATEGORIA" }
+    3. return: Dados da nova categoria **id** , **nome** , **usuario_id**, **usuario** (**id** , **nome**, **email**, **login**, **senha**)
+
+4.  `PUT /categorias/:id`
+    1. parametro: **id** do categoria desejada
+    2. body: { "nome": "NOME_CATEGORIA", "usuario_id": "USUARIO_ID_CATEGORIA" }
+    3. return: Dados atualizados da categoria **id** , **nome** , **usuario_id**, **usuario** (**id** , **nome**, **email**, **login**, **senha**)
+
+5.  `DELETE /categorias/:id`
+    1. parametro: **id** da categoria que desejado excluir
+    2. body: NULL
+    3. return: **Categoria excluído**, caso true. **Erro ao excluir categoria**, case false
+
+##Tarefa
+
+
+1.  `GET /tarefas/:id`
+    1. parametro: **id** do tarefa desejada
+    2. body: NULL
+    3. return: O **id** , **nome** , **usuario_id**, **usuario** (**id** , **nome**, **email**, **login**, **senha**) da tarefa selecionada
+
+2.  `GET /tarefas`
+    1. parametro: NULL
+    2. body: NULL
+    3. return: Os **id** , **nome** , **usuario_id**, **usuario** (**id** , **nome**, **email**, **login**, **senha**) de todas as tarefas
+
+3.  `POST /tarefas`
+    1. parametro: NULL
+    2. body: { "nome": "NOME_CATEGORIA", "usuario_id": "USUARIO_ID_CATEGORIA" }
+    3. return: Dados da nova tarefa **id** , **nome** , **usuario_id**, **usuario** (**id** , **nome**, **email**, **login**, **senha**)
+
+4.  `PUT /tarefas/:id`
+    1. parametro: **id** do tarefa desejada
+    2. body: { "nome": "NOME_CATEGORIA", "usuario_id": "USUARIO_ID_CATEGORIA" }
+    3. return: Dados atualizados da tarefa **id** , **nome** , **usuario_id**, **usuario** (**id** , **nome**, **email**, **login**, **senha**)
+
+5.  `DELETE /tarefas/:id`
+    1. parametro: **id** da tarefa que desejado excluir
+    2. body: NULL
+    3. return: **Tarefa excluído**, caso true. **Erro ao excluir tarefa**, case false
